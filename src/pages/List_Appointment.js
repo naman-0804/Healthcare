a660b930f6dde36fe3bad20573d353caf0a7a653
Naman Srivastava <namansrivastava1608@gmail.com>
2024-04-13 19:14:23 +0530

    Added Appointement list

---
+++ src/pages/List_Appointment.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import supabase from '../config/supabaseClient';
+
+function ListAppointments() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      try {
+        // Fetch appointments data from the appointments table
+        const { data, error } = await supabase.from('appointement').select('*');
+        if (data) {
+          setAppointments(data);
+        }
+        if (error) {
+          throw error;
+        }
+      } catch (error) {
+        console.error('Error fetching appointments:', error.message);
+      }
+    };
+
+    fetchAppointments();
+  }, []);
+
+  return (
+    <div>
+      <h2>Appointments</h2>
+      <ul>
+        {appointments.map(appointment => (
+          <li key={appointment.id}>
+            <p>Date: {appointment.date}</p>
+            <p>Department: {appointment.dept}</p>
+            {/* Add more details here as needed */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ListAppointments;
